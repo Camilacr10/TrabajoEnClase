9acb46670e6cf0812dc1dd98bee7a4bed405ea7b
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-04-09 19:11:46 -0600

    Creación del proyecto

---
+++ TrabajoEnClase/TrabajoEnClase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabajoEnClase.Startup))]
+namespace TrabajoEnClase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
