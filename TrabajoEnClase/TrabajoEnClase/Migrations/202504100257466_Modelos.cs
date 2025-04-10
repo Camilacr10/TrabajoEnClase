d1279bb09ee7db3a3b127845419f4e6dbdfadd93
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2025-04-09 21:05:00 -0600

    Migracion Usuarios

---
+++ TrabajoEnClase/TrabajoEnClase/Migrations/202504100257466_Modelos.cs
@@ -0,0 +1,16 @@
+﻿namespace TrabajoEnClase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Modelos : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ TrabajoEnClase/TrabajoEnClase/Models/TrabajoEnClaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TrabajoEnClase.Models
+{
+    public class TrabajoEnClaseContext : DbContext
+    {
+
+	public TrabajoEnClaseContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Mascota> Mascotas { get; set; }
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<HistorialMedico> HistorialesMedicos { get; set; }
+    }
+}
