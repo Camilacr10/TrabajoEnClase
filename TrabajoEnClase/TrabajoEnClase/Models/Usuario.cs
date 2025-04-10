7fa9df519b771d1cee7d99a095e78b4d9403e42f
Vale-Torres05 <113872652+Vale-Torres05@users.noreply.github.com>
2025-04-09 20:26:09 -0600

    Modelo usuario

---
+++ TrabajoEnClase/TrabajoEnClase/Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Usuario
+{
+    public int Id_Usuario { get; set; }
+
+    [Required]
+    public string NombreUsuario { get; set; }
+
+    [Required]
+    public string NombreCompleto { get; set; }
+
+    [Required, EmailAddress]
+    public string Correo { get; set; }
+
+    public string Telefono { get; set; }
+
+    [Required]
+    public string Contrasena { get; set; }
+
+    [Required]
+    public string Rol { get; set; }
+
+    public virtual ICollection<Mascota> MascotasPropietario { get; set; }
+    public virtual ICollection<Mascota> MascotasRegistradas { get; set; }
+    public virtual ICollection<Cita> CitasVeterinario { get; set; }
+}
+
